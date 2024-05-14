90bdd263921a6189fc6f15d43ff7b8d3e0032af4
Robin Ochoa <robin8a@gmail.com>
2024-05-14 16:08:54 -0500

    update

---
+++ 00_INCEPTION/001_DESIGN/script.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(showSlide, 3000, currentSlide + 1); // Auto-rotate every 3 seconds
